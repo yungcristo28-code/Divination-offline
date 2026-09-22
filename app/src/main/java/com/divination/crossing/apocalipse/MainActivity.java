c0e3d2b1f6dcff4a1977c68716ebf3bca573d784
yungcristo28-code <yungcristo28@gmail.com>
2026-09-22 18:10:34 -0300

    Create MainActivity.java

---
+++ app/src/main/java/com/divination/crossing/apocalipse/MainActivity.java
@@ -0,0 +1,20 @@
+package com.divination.crossing.apocalipse;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        setContentView(new GameView(this));
+    }
+}
