88d940c8a1166dc6e8b9ef81455976f316ea64d1
yungcristo28-code <yungcristo28@gmail.com>
2026-09-22 19:38:21 -0300

    Add DamageText class for displaying damage information

---
+++ app/src/main/java/com/divination/crossing/apocalipse/DamageText.java
@@ -0,0 +1,24 @@
+package com.divination.crossing.apocalipse;
+
+public class DamageText {
+
+    private float x;
+    private float y;
+
+    private int damage;
+
+    private int life;
+
+    private boolean active;
+
+
+    public DamageText(
+            float x,
+            float y,
+            int damage
+    ){
+
+        this.x = x;
+        this.y = y;
+
+        this.damage
