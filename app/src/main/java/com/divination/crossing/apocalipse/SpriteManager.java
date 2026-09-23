0b3d016a73797eb4b33597914fa3dc69c2812d35
yungcristo28-code <yungcristo28@gmail.com>
2026-09-22 22:19:34 -0300

    Refactor SpriteManager to include igorRun sprite

--- app/src/main/java/com/divination/crossing/apocalipse/SpriteManager.java
+++ app/src/main/java/com/divination/crossing/apocalipse/SpriteManager.java
@@ -1,29 +1,29 @@
 package com.divination.crossing.apocalipse;
 
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.content.Context;
 
 
 
 public class SpriteManager {
 
 
-
     private static SpriteManager instance;
 
 
+    private Context context;
+
+
+
+    // ==========================
+    // SPRITES DO IGOR
+    // ==========================
 
     private Bitmap igorIdle;
-
     private Bitmap igorAttack;
-
-    private Bitmap igorHit;
-
-
-
-    private Context context;
+    private Bitmap igorRun;
 
 
 
@@ -33,12 +33,10 @@ public class SpriteManager {
             Context context
     ){
 
-        this.context = context;
-
+        this.context =
+                context.getApplicationContext();
 
     }
-
-
 
 
 
@@ -49,22 +47,17 @@ public class SpriteManager {
     ){
 
 
-
         if(instance == null){
-
 
             instance =
                     new SpriteManager(
                             context
                     );
 
-
         }
 
 
-
         return instance;
-
 
     }
 
@@ -73,70 +66,40 @@ public class SpriteManager {
 
 
 
+    // ==========================
+    // CARREGAR SPRITES
+    // ==========================
 
 
     public void loadSprites(){
 
 
+        /*
+        
+        Futuro:
 
-        try {
-
-
-
-            igorIdle =
-                    BitmapFactory.decodeResource(
-                            context.getResources(),
-                            context.getResources()
-                                    .getIdentifier(
-                                            "igor_idle",
-                                            "drawable",
-                                            context.getPackageName()
-                                    )
-                    );
+        igorIdle =
+        BitmapFactory.decodeResource(
+                context.getResources(),
+                R.drawable.igor_idle
+        );
 
 
-
-            igorAttack =
-                    BitmapFactory.decodeResource(
-                            context.getResources(),
-                            context.getResources()
-                                    .getIdentifier(
-                                            "igor_attack",
-                                            "drawable",
-                                            context.getPackageName()
-                                    )
-                    );
+        igorAttack =
+        BitmapFactory.decodeResource(
+                context.getResources(),
+                R.drawable.igor_attack
+        );
 
 
-
-            igorHit =
-                    BitmapFactory.decodeResource(
-                            context.getResources(),
-                            context.getResources()
-                                    .getIdentifier(
-                                            "igor_hit",
-                                            "drawable",
-                                            context.getPackageName()
-                                    )
-                    );
+        igorRun =
+        BitmapFactory.decodeResource(
+                context.getResources(),
+                R.drawable.igor_run
+        );
 
 
-
-        }
-
-        catch(Exception e){
-
-
-
-            igorIdle = null;
-
-            igorAttack = null;
-
-            igorHit = null;
-
-
-        }
-
+        */
 
 
     }
@@ -146,42 +109,32 @@ public class SpriteManager {
 
 
 
+    // ==========================
+    // GETTERS
+    // ==========================
 
 
     public Bitmap getIgorIdle(){
 
-
         return igorIdle;
 
-
     }
-
-
-
 
 
 
     public Bitmap getIgorAttack(){
 
-
         return igorAttack;
 
-
     }
 
 
 
+    public Bitmap getIgorRun(){
 
-
-
-    public Bitmap getIgorHit(){
-
-
-        return igorHit;
-
+        return igorRun;
 
     }
-
 
 
 
